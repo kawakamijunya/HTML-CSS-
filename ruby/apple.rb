d86ba2150c7f69b197720a8c6d6b865efb8edc63
EC2 Default User <radball0506@gmail.com>
2023-03-29 09:54:10 +0000

    フローチャートからコードを書いてみよう

---
+++ ruby/apple.rb
@@ -0,0 +1,13 @@
+apple="Yamanashi"#この行を追加
+
+if apple=="Aomori"
+  puts "このリンゴは青森県さんです"
+  elsif apple=="Nagano"
+    puts "このリンゴは青森県産ではなく、長野県産です"
+  else
+    puts "このリンゴは青森県産でも長野県産でもありません"
+  end
+  
+      
+      
+      
